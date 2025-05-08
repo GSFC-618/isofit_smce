d382983112b074929cdc16bd9f09e3b3fc11d558
Shawn P. Serbin <shawn.p.serbin@nasa.gov>
2025-05-08 16:54:55 -0400

    Updates

--- scripts/build_isofit.sh
+++ scripts/build_isofit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Build the ISOFIT docker image without the embedded datasets
+#docker buildx build --push -t serbinsh/isofit_smce --no-cache \
+#--platform linux/amd64,linux/arm64 -f docker/Dockerfile .
+
 docker buildx build --push -t serbinsh/isofit_smce --no-cache \
---platform linux/amd64,linux/arm64 -f docker/Dockerfile .
+--platform linux/amd64 -f docker/Dockerfile .
 
 ## EOF
