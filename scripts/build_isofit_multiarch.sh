1f14c165a478e7470eb1ff2369a62a133a26fa2c
Shawn P. Serbin <shawn.p.serbin@nasa.gov>
2025-05-09 12:11:52 -0400

    Updated builds. Still a work in progress with tagging and versioning between the SMCE version and a standalone multiarch version for local machines

--- scripts/build_isofit_multiarch.sh
+++ scripts/build_isofit_multiarch.sh
@@ -4,7 +4,7 @@
 #docker buildx build --push -t serbinsh/isofit_smce --no-cache \
 #--platform linux/amd64,linux/arm64 -f docker/Dockerfile .
 
-docker buildx build --push -t serbinsh/isofit_smce:smce --no-cache \
---platform linux/amd64 -f docker/Dockerfile .
+docker buildx build --push -t serbinsh/isofit_smce:multiarch --no-cache \
+--platform linux/amd64,linux/arm64 -f docker/Dockerfile .
 
 ## EOF
