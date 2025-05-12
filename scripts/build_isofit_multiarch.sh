0553cb7061b4fd1577b5db21a58bf3e8383e9f47
Shawn P. Serbin <shawn.p.serbin@nasa.gov>
2025-05-12 10:33:56 -0400

    Updates to build and tagging.  Preparing for different builds for stand alone and to run on airborne SMCE

--- scripts/build_isofit_multiarch.sh
+++ scripts/build_isofit_multiarch.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# Build the ISOFIT docker image without the embedded datasets
-#docker buildx build --push -t serbinsh/isofit_smce --no-cache \
-#--platform linux/amd64,linux/arm64 -f docker/Dockerfile .
+cd isofit
+echo $PWD
+CI_COMMIT_TAG=$(git describe --tags --abbrev=0)
+echo "Latest ISOFIT release: ${CI_COMMIT_TAG}"
+cd ..
+echo $PWD
 
-docker buildx build --push -t serbinsh/isofit_smce:multiarch --no-cache \
---platform linux/amd64,linux/arm64 -f docker/Dockerfile .
+# Create tag
+CI_COMMIT_TAG_DOCKER="multiarch_"${CI_COMMIT_TAG}
+echo "Docker ISOFIT tag: ${CI_COMMIT_TAG_DOCKER}"
+
+echo ""
+echo ""
+echo "Building new ISOFIT image: ${CI_COMMIT_TAG_DOCKER}"
+echo "************************************************************************************"
+sleep 1
+
+# Build the ISOFIT docker image without the embedded datasets
+# Multi-arch version - what additional architectures should be included here?
+#docker buildx build --push -t serbinsh/isofit_smce:multiarch --no-cache \
+#--platform linux/amd64,linux/arm64 -f docker/Dockerfile_standalone .
+docker buildx build --push -t serbinsh/isofit_smce:$CI_COMMIT_TAG_DOCKER --no-cache \
+--platform linux/amd64,linux/arm64 -f docker/Dockerfile_standalone .
 
 ## EOF
