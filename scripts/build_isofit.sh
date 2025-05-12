78e6355f8b05784a5ca224598ca72f3b0cb2dfa9
Shawn P. Serbin <shawn.p.serbin@nasa.gov>
2025-05-12 10:42:46 -0400

    Updates

--- scripts/build_isofit.sh
+++ scripts/build_isofit.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-# Build the ISOFIT docker image without the embedded datasets
-#docker buildx build --push -t serbinsh/isofit_smce --no-cache \
-#--platform linux/amd64,linux/arm64 -f docker/Dockerfile .
+cd isofit
+echo $PWD
+CI_COMMIT_TAG=$(git describe --tags --abbrev=0)
+echo "Latest ISOFIT release: ${CI_COMMIT_TAG}"
+cd ..
+echo $PWD
 
+# Create tag
+CI_COMMIT_TAG_DOCKER="multiarch_"${CI_COMMIT_TAG}
+echo "Docker ISOFIT tag: ${CI_COMMIT_TAG_DOCKER}"
+
+echo ""
+echo ""
+echo "Building new ISOFIT image: ${CI_COMMIT_TAG_DOCKER}"
+echo "************************************************************************************"
+sleep 1
+
+# Build the ISOFIT docker image without the embedded datasets
 docker buildx build --push -t serbinsh/isofit_smce:smce --no-cache \
 --platform linux/amd64 -f docker/Dockerfile .
 
