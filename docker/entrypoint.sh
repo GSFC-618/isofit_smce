82177dba64497663d2ecc5a49a9d29852776b221
Shawn P. Serbin <shawn.p.serbin@nasa.gov>
2025-05-16 10:06:53 -0400

    Updates

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,4 +12,14 @@ export LD_PRELOAD=$(find /usr/lib -name libnss_wrapper.so | head -n1)
 
 echo "${NB_USER}:x:${NB_UID}:${NB_GID}:${NB_USER}:${HOME}:/bin/bash" > $NSS_WRAPPER_PASSWD
 
+# Initialize mamba - attempt to make this permanent, however will it continue
+# to add to home every time a user logs in?
+# Confirmed - this will append the command every time its launched
+#echo 'eval "$(mamba shell hook --shell bash)"' >> ~/.bashrc
+
+# Set specific conda env - may not want to do this since it will be set
+# in users home across all hub instances, some of which wont have the 
+# isofit env
+#echo "mamba activate isofit" >> ~/.bashrc
+
 exec "$@"
